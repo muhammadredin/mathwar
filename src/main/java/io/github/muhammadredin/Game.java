cecb070f6d1d069a89215d3a57bf520730cac2ff
Enigma <muhammadredin.b@gmail.com>
2024-08-30 20:59:55 +0700

    Added timeout to input when user try to solving the math question

--- src/main/java/io/github/muhammadredin/Game.java
+++ src/main/java/io/github/muhammadredin/Game.java
@@ -2,6 +2,7 @@ package io.github.muhammadredin;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 enum GameState {
     START, GAME_OVER;
@@ -23,6 +24,26 @@ public class Game {
         return gameState;
     }
 
+    public static Integer getInputWithTimeout(int timeoutSeconds) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Integer> future = executor.submit(() -> {
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextInt();
+        });
+
+        try {
+            return future.get(timeoutSeconds, TimeUnit.SECONDS);
+        } catch (TimeoutException e) {
+            future.cancel(true); // Cancel the input task
+            return 0; // Indicate timeout
+        } catch (InterruptedException | ExecutionException e) {
+            Thread.currentThread().interrupt(); // Restore interrupted status
+            return 0;
+        } finally {
+            executor.shutdown(); // Shutdown the executor
+        }
+    }
+
     public void turn(Entity player, Entity cpu) {
         Random rand = new Random();
         Scanner input = new Scanner(System.in);
@@ -30,20 +51,27 @@ public class Game {
             int number1 = rand.nextInt(100);
             int number2 = rand.nextInt(100);
             int answer = number1 + number2;
+            System.out.println("Solve This to Attack: " + number1 + " + " + number2);
+            int userInput = getInputWithTimeout(5);
 
-            System.out.println("Solve This to Attack: " + number1 + " " + number2);
-            int userInput = input.nextInt();
 
-            if (answer == userInput) {
-                player.attack(cpu);
-                System.out.println("You attacked!");
-                System.out.println(cpu.getName() + " HP left: " +
-                        cpu.getHpInfo());
-            } else {
+            if (userInput == 0) {
                 cpu.attack(player);
                 System.out.println("You've been attacked!");
                 System.out.println(player.getName() + " HP left: " +
                         player.getHpInfo());
+            } else {
+                if (answer == userInput) {
+                    player.attack(cpu);
+                    System.out.println("You attacked!");
+                    System.out.println(cpu.getName() + " HP left: " +
+                            cpu.getHpInfo());
+                } else {
+                    cpu.attack(player);
+                    System.out.println("You've been attacked!");
+                    System.out.println(player.getName() + " HP left: " +
+                            player.getHpInfo());
+                }
             }
         }
     }
--- src/main/java/io/github/muhammadredin/Main.java
+++ src/main/java/io/github/muhammadredin/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Game game = new Game();
+
         Hero hero = new Hero("Redin");
         Goblin goblin = new Goblin();
+
         while (game.getGameState() == GameState.START) {
             // Game control
             game.turn(hero, goblin);
 
-            if(hero.getHpInfo() == 0) {
+            if (hero.getHpInfo() == 0) {
                 System.out.println("GAME OVER!!! YOU LOSE!!!");
                 game.endGame();
             } else if (goblin.getHpInfo() == 0) {
